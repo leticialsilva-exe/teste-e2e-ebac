ca7dd2e35f4de9104880c006008475fa2f6aaed5
leticialsilva-exe <leticial.silva@outlook.com>
2025-07-29 22:58:07 -0300

    adjusting for github pages

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,12 @@ module.exports = defineConfig({
     },
     baseUrl: "http://lojaebac.ebaconline.art.br",
     "reporter": "mochawesome",
+    "reporterOptions": {
+      "reportDir": "mochawesome-report",
+      "reportFilename": "index.html",
+      "overwrite": false,
+      "html": true,
+      "json": false
+    }
   },
 });
